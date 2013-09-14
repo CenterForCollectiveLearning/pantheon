2c9ec2b88b5ba120cdc9dfdda98689df60431f53
Kevin Hu <kzh@dhcp-18-111-3-65.dyn.mit.edu>
2013-09-14 15:58:48 -0400

    Some parameterized routing

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -1,13 +1,33 @@
 
 Router.map(function() {
     this.route('observatory',
-        {path: '/'} // :vizType/:tradeflow/:ent1/:ent2/:from/:to/:langs'}
+        {path: '/'}  // :vizType/:ent1/:ent2/:from/:to/:langs'}
     );
-    this.route('observatory',
-        {path: '/:vizType'}
+    this.route('observatory', {
+        path: '/:vizType',
+        data: function() { 
+            Session.set('vizType', this.params.vizType); 
+        }}
     );
-    this.route('observatory',
-        {path: '/:vizType/:tradeflow'}
+    this.route('observatory', {
+        path: '/:vizType/:ent1/:ent2',
+        data: function() { 
+            Session.set('vizType', this.params.vizType); 
+            Session.set('ent1', this.params.ent1);  
+            Session.set('ent2', this.params.ent2);  
+        }}
+    );
+
+    this.route('observatory', {
+        path: '/:vizType/:ent1/:ent2/:from/:to/:langs',
+        data: function() { 
+            Session.set('vizType', this.params.vizType); 
+            Session.set('ent1', this.params.ent1);  
+            Session.set('ent2', this.params.ent2);  
+            Session.set('from', this.params.from); 
+            Session.set('to', this.params.to);  
+            Session.set('langs', this.params.langs);  
+        }}
     );
     // this.route('observatory'); // TODO: there is probaby a way to make two paths point to the same template
     this.route('vision');
