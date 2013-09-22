4668b70910e17860f0156996aebae0e442056e87
Amy <amy_yu@mit.edu>
2013-09-22 16:38:53 -0400

    treemap.js

--- client/views/treemap.js
+++ client/views/treemap.js
@@ -1,15 +1,14 @@
-/*
 Template.treemap.dataReady = function() {
-    return treemapSub.ready()
+    return treemapSub.ready();
 }
-
-Template.tooltip.tooltip = function() {
-    return Session.get("tooltipIndustry");
-}
-
-Template.tooltip.tooltip_data = function() {
-    return Domains.findOne({ _id: Session.get("tooltipIndustry") })
-}
+//
+//Template.tooltip.tooltip = function() {
+//    return Session.get("tooltipIndustry");
+//}
+//
+//Template.tooltip.tooltip_data = function() {
+//    return Domains.findOne({ _id: Session.get("tooltipIndustry") })
+//}
 
 var treeProps = {
     width: 725,
@@ -18,97 +17,101 @@ var treeProps = {
 
 Template.treemap_svg.properties = treeProps;
 
-var color_domain = d3.scale.ordinal()
+var color_domains = d3.scale.ordinal()
     .domain(["INSTITUTIONS", "ARTS", "HUMANITIES", "BUSINESS & LAW", "EXPLORATION", "PUBLIC FIGURE", "SCIENCE & TECHNOLOGY", "SPORTS"])
     .range(["#ECD078", "#D95B43", "#43c1d9", "#C02942", "#546c97", "#d278c2", "#53a9f1", "#79BD9A"]);
 
-var color_domain_languages = d3.scale.ordinal()
+var color_languages = d3.scale.ordinal()
     .domain(["Afro-Asiatic", "Altaic", "Austro-Asiatic", "Austronesian", "Basque", "Caucasian", "Creoles and pidgins", "Dravidian", "Eskimo-Aleut", "Indo-European", "Niger-Kordofanian", "North American Indian", "Sino-Tibetan", "South American Indian", "Tai", "Uralic"])
     .range(["#E0BA9B", "#D95B43", "#43c1d9", "#C02942", "#546c97", "#d278c2", "#53a9f1", "#79BD9A", "#A69E80", "#ECD078", "#D28574", "#E7EDEA", "#CEECEF", "#912D1D", "#DE7838", "#59AB6D"]);
 
-var color_domain_countries = d3.scale.ordinal()
+var color_countries = d3.scale.ordinal()
     .domain(["Africa", "Asia", "Europe", "North America", "South America", "Oceania"])
     .range(["#E0BA9B", "#D95B43", "#43c1d9", "#C02942", "#546c97", "#d278c2"]);
 
 Template.treemap_svg.rendered = function() {
-    // Don't re-render with the same parameters...?
+// Don't re-render with the same parameters...?
+    var context = this;
     if( this.rendered ) return;
     this.rendered = true;
+    var viz = vizwhiz.viz() ;
+    var attr = Domains.find().fetch();
+    var data = Treemap.find().fetch();
 
-    var nested = d3.nest()
-        .key(function(d) { return d.domain })
-        .key(function(d) { return d.industry })
-        .entries(Domains.find().fetch())
+    var attrs = {};
 
-    var treemap = d3.layout.treemap()
-        .padding(0)
-        .size([treeProps.width, treeProps.height])
-        .value(function (d) {
-            return d.count;
-        })
-        .children(function(d) {
-            return d.values;
-        });
+    attr.forEach(function(a){
+        var dom = a.domain;
+        var ind = a.industry;
+        var occ = a.occupation;
+        var dom_color = color_domains(dom.toUpperCase());
+        var domDict = {
+            id: dom
+            , name: dom
+        };
+        var indDict = {
+            id: ind
+            , name: ind
+        };
+        var occDict = {
+            id: occ
+            , name: occ
+        };
+        attrs[dom] = {
+            id: dom
+            , name: dom
+            , color: dom_color
+            , nesting_1: domDict
+        };
+        attrs[ind] = {
+            id: ind
+            , name: ind
+            , color: dom_color
+            , nesting_1: domDict
+            , nesting_3: indDict
+        };
+        attrs[occ] = {
+            id: occ
+            , name: occ
+            , color: dom_color
+            , nesting_1: domDict
+            , nesting_3: indDict
+            , nesting_5: occDict
+        };
+    });
 
-    var svg = d3.select(this.find("svg"))
-        .append("g")
-            .attr("transform", "translate(-.5,-.5)");
+    var flat = [];
+    data.forEach(function(d){
+       flat.push({"id": d.occupation, "name": d.occupation, "num_ppl": d.count, "year":2000});  //use a dummy year here for now ...
+    });
 
-    var data = {
-        "key": "-",
-        "values": nested
-    };
+    console.log("ATTRS:");
+    console.log(attrs);
+    console.log("DATA:")
+    console.log(flat);
 
-    var cell = svg.datum(data)
-        .selectAll("g")
-        .data(treemap.nodes)
-        .enter()
-    .append("g")
-        .attr("class", function (d) {
-            return (!d.values || d.values.length === 0) ? "leaf cell" : "parent cell";
-        })
-        .attr("transform", function (d) {
-            return "translate(" + d.x + "," + d.y + ")";
-        });
+    viz
+        .type("tree_map")
+//        .dev(true)
+        .width(725)
+        .height(560)
+        .id_var("id")
+        .attrs(attrs)
+        .text_var("name")
+        .value_var("num_ppl")
+        .tooltip_info({})
+        .total_bar({"prefix": "Total Exports: ", "suffix": " individuals"})
+        .nesting(["nesting_1","nesting_3","nesting_5"])
+        .depth("nesting_3")
+        .font("PT Sans")
+        .font_weight("lighter")
+        .color_var("color");
 
-    cell.append("rect")
-        .attr("width", function (d) {
-            return d.dx;
-        })
-        .attr("height", function (d) {
-            return d.dy;
-        })
-        .style("cursor", "pointer")
-        .style("fill", function (d) {
-            if (d.depth === 3) {
-                return color_domain(d.parent.parent.key);
-            }
-            else if (d.depth === 2) {
-                return color_domain(d.parent.key);
-            }
-            else {
-                return null;
-            }
-        });
+    d3.select(context.find("svg"))
+        .datum(flat)
+        .call(viz);
 
-    // Add text to cells
-    cell.append("text")
-        .attr("text-anchor", "start")
-        .attr('x', '0.2em')
-        .attr('y', '0.1em')
-        .attr('dy', '1em')
-        .attr("font-size", function (d) {
-            var size = (d.dx) / 7
-            if (d.dx < d.dy) var size = d.dx / 7
-            else var size = d.dy / 7
-            return size;
-        })
-        .style("cursor", "pointer")
-        .text(function (d) {
-            return d.values ? null : d.industry; // TODO don't hardcode this!
-        })
-        .each(wordWrap);
-
+   /*
     d3.selectAll(".leaf rect").on("mouseover", function (d) {
         // TODO generalize this for other treemaps later
 
@@ -116,64 +119,5 @@ Template.treemap_svg.rendered = function() {
 
         $("#tooltip").css("left", (d3.event.pageX + 90) + "px").css("top", (d3.event.pageY - 95) + "px");
     });
+    */
 }
-
-function wordWrap(d) {
-    if (this.firstChild == null) return;
-
-    var words = d.industry.split(' ');
-    var line = new Array();
-    var length = 0;
-    var text = "";
-    var width = d.dx;
-    var height = d.dy;
-    var word;
-
-    while (words.length) {
-        word = words.shift();
-        line.push(word);
-        if (words.length) this.firstChild.data = line.join(' ') + " " + words[0];
-        else this.firstChild.data = line.join(' ');
-        length = this.getBBox().width;
-        if (length < width && words.length) {;
-        } else {
-//            console.log(word);
-            text = line.join(' ');
-            this.firstChild.data = text;
-            if (this.getBBox().width > width) {
-                text = d3.select(this).select(function () {
-                    return this.lastChild;
-                }).text();
-                text = text + "...";
-                d3.select(this).select(function () {
-                    return this.lastChild;
-                }).text(text);
-                d3.select(this).classed("wordwrapped", true);
-                break;
-            }
-
-            if (text != '') {
-                d3.select(this).append("svg:tspan").attr("x", 0).attr("dx", "0.15em").attr("dy", "0.9em").text(text);
-            }
-
-            if (this.getBBox().height > height && words.length) {
-                text = d3.select(this).select(function () {
-                    return this.lastChild;
-                }).text();
-                text = text + "...";
-                d3.select(this).select(function () {
-                    return this.lastChild;
-                }).text(text);
-                d3.select(this).classed("wordwrapped", true);
-
-                break;
-            }
-
-            line = new Array();
-        }
-    }
-
-    this.firstChild.data = '';
-}
-
-*/
---
+++ client/views/treemap_old.js
@@ -0,0 +1,179 @@
+/*
+Template.treemap.dataReady = function() {
+    return treemapSub.ready()
+}
+
+Template.tooltip.tooltip = function() {
+    return Session.get("tooltipIndustry");
+}
+
+Template.tooltip.tooltip_data = function() {
+    return Domains.findOne({ _id: Session.get("tooltipIndustry") })
+}
+
+var treeProps = {
+    width: 725,
+    height: 560
+};
+
+Template.treemap_svg.properties = treeProps;
+
+var color_domain = d3.scale.ordinal()
+    .domain(["INSTITUTIONS", "ARTS", "HUMANITIES", "BUSINESS & LAW", "EXPLORATION", "PUBLIC FIGURE", "SCIENCE & TECHNOLOGY", "SPORTS"])
+    .range(["#ECD078", "#D95B43", "#43c1d9", "#C02942", "#546c97", "#d278c2", "#53a9f1", "#79BD9A"]);
+
+var color_domain_languages = d3.scale.ordinal()
+    .domain(["Afro-Asiatic", "Altaic", "Austro-Asiatic", "Austronesian", "Basque", "Caucasian", "Creoles and pidgins", "Dravidian", "Eskimo-Aleut", "Indo-European", "Niger-Kordofanian", "North American Indian", "Sino-Tibetan", "South American Indian", "Tai", "Uralic"])
+    .range(["#E0BA9B", "#D95B43", "#43c1d9", "#C02942", "#546c97", "#d278c2", "#53a9f1", "#79BD9A", "#A69E80", "#ECD078", "#D28574", "#E7EDEA", "#CEECEF", "#912D1D", "#DE7838", "#59AB6D"]);
+
+var color_domain_countries = d3.scale.ordinal()
+    .domain(["Africa", "Asia", "Europe", "North America", "South America", "Oceania"])
+    .range(["#E0BA9B", "#D95B43", "#43c1d9", "#C02942", "#546c97", "#d278c2"]);
+
+Template.treemap_svg.rendered = function() {
+    // Don't re-render with the same parameters...?
+    if( this.rendered ) return;
+    this.rendered = true;
+
+    var nested = d3.nest()
+        .key(function(d) { return d.domain })
+        .key(function(d) { return d.industry })
+        .entries(Domains.find().fetch())
+
+    var treemap = d3.layout.treemap()
+        .padding(0)
+        .size([treeProps.width, treeProps.height])
+        .value(function (d) {
+            return d.count;
+        })
+        .children(function(d) {
+            return d.values;
+        });
+
+    var svg = d3.select(this.find("svg"))
+        .append("g")
+            .attr("transform", "translate(-.5,-.5)");
+
+    var data = {
+        "key": "-",
+        "values": nested
+    };
+
+    var cell = svg.datum(data)
+        .selectAll("g")
+        .data(treemap.nodes)
+        .enter()
+    .append("g")
+        .attr("class", function (d) {
+            return (!d.values || d.values.length === 0) ? "leaf cell" : "parent cell";
+        })
+        .attr("transform", function (d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        });
+
+    cell.append("rect")
+        .attr("width", function (d) {
+            return d.dx;
+        })
+        .attr("height", function (d) {
+            return d.dy;
+        })
+        .style("cursor", "pointer")
+        .style("fill", function (d) {
+            if (d.depth === 3) {
+                return color_domain(d.parent.parent.key);
+            }
+            else if (d.depth === 2) {
+                return color_domain(d.parent.key);
+            }
+            else {
+                return null;
+            }
+        });
+
+    // Add text to cells
+    cell.append("text")
+        .attr("text-anchor", "start")
+        .attr('x', '0.2em')
+        .attr('y', '0.1em')
+        .attr('dy', '1em')
+        .attr("font-size", function (d) {
+            var size = (d.dx) / 7
+            if (d.dx < d.dy) var size = d.dx / 7
+            else var size = d.dy / 7
+            return size;
+        })
+        .style("cursor", "pointer")
+        .text(function (d) {
+            return d.values ? null : d.industry; // TODO don't hardcode this!
+        })
+        .each(wordWrap);
+
+    d3.selectAll(".leaf rect").on("mouseover", function (d) {
+        // TODO generalize this for other treemaps later
+
+        Session.set("tooltipIndustry", d._id);
+
+        $("#tooltip").css("left", (d3.event.pageX + 90) + "px").css("top", (d3.event.pageY - 95) + "px");
+    });
+}
+
+function wordWrap(d) {
+    if (this.firstChild == null) return;
+
+    var words = d.industry.split(' ');
+    var line = new Array();
+    var length = 0;
+    var text = "";
+    var width = d.dx;
+    var height = d.dy;
+    var word;
+
+    while (words.length) {
+        word = words.shift();
+        line.push(word);
+        if (words.length) this.firstChild.data = line.join(' ') + " " + words[0];
+        else this.firstChild.data = line.join(' ');
+        length = this.getBBox().width;
+        if (length < width && words.length) {;
+        } else {
+//            console.log(word);
+            text = line.join(' ');
+            this.firstChild.data = text;
+            if (this.getBBox().width > width) {
+                text = d3.select(this).select(function () {
+                    return this.lastChild;
+                }).text();
+                text = text + "...";
+                d3.select(this).select(function () {
+                    return this.lastChild;
+                }).text(text);
+                d3.select(this).classed("wordwrapped", true);
+                break;
+            }
+
+            if (text != '') {
+                d3.select(this).append("svg:tspan").attr("x", 0).attr("dx", "0.15em").attr("dy", "0.9em").text(text);
+            }
+
+            if (this.getBBox().height > height && words.length) {
+                text = d3.select(this).select(function () {
+                    return this.lastChild;
+                }).text();
+                text = text + "...";
+                d3.select(this).select(function () {
+                    return this.lastChild;
+                }).text(text);
+                d3.select(this).classed("wordwrapped", true);
+
+                break;
+            }
+
+            line = new Array();
+        }
+    }
+
+    this.firstChild.data = '';
+}
+
+*/
