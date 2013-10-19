04f665fc712a2761b9b4d220ba57e002738d8c30
Amy <amy_yu@mit.edu>
2013-10-19 18:28:28 -0400

    updated peopleTopN publication to return all attributes

--- server/top_level_pub.js
+++ server/top_level_pub.js
@@ -61,10 +61,10 @@ Meteor.publish("peopletopN", function(begin, end, L, country, domain, N) {
     var collectionName = "topNpeople";
 
     var criteria = getCountryExportArgs(begin, end, L, country);
-    var projection = {
-        fields: {_id: 1}, //only get the ids of the people - look up the people in the client (from allpeople)
-        sort: {numlangs: -1}
-    };
+//    var projection = {
+//        fields: {_id: 1}, //only get the ids of the people - look up the people in the client (from allpeople)
+//        sort: {numlangs: -1}
+//    };
 
     if(N !== 'all'){
         projection.limit = N;
@@ -74,7 +74,7 @@ Meteor.publish("peopletopN", function(begin, end, L, country, domain, N) {
         criteria.$or = [{domain:domain.substring(1)}, {industry:domain.substring(1)}, {occupation:domain.substring(1)}];
     };
 
-    People.find(criteria, projection).forEach(function(person){
+    People.find(criteria).forEach(function(person){
             sub.added(collectionName, person._id, person);
         });
 
