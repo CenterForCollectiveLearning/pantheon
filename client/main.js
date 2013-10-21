1998da502b1507b9a87f4369a1f8f5da315fa467
Kevin Hu <kzh@dhcp-18-85-26-144.media.mit.edu>
2013-10-21 19:23:13 -0400

    Select ranking

--- client/main.js
+++ client/main.js
@@ -133,19 +133,8 @@ Template.google_analytics.rendered = function() {
 
     ga('create', 'UA-44888546-1', 'mit.edu');
     ga('send', 'pageview');
-
-// (function(i,s,o,g,r,a,m){
-//     console.log(i,s,o,g,r,a,m);
-//     i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-//       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-//       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-//   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 }
 
-// Select sections based on template
-Template.nav.selected = function() {
-    return Session.equals('page', this._id) ? 'selected_section' : '';
-}
 
 // Section Navigation
 // TODO Is this repetitiveness necessary for correct formatting?
@@ -158,7 +147,7 @@ var left_sections = [
     {
         section: "Rankings",
         template: "rankings",
-        url: "/ranking"
+        url: "/rankings"
     },
     // {
     //     section: "People",
