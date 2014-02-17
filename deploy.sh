d8af09bf9a8704cf37d965b3d280f007dff1566f
Amy Yu <amyzhao.yu@gmail.com>
2014-02-17 13:57:49 -0500

    Merged some more changes on production

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-echo Bundling meteor project
-meteor bundle ../bundle.tgz
-echo Untarring bundle
-tar -zxvf ../bundle.tgz
-# PORT=80 MONGO_URL=mongodb://localhost:27017/localhost ROOT_URL=http://localhost node ../bundle/main.js
